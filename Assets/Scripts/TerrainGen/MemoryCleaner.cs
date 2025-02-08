090a4171b4b3e2c9839568c3086c0932b15c6c1f
JonatanK <164728285+J0natanK@users.noreply.github.com>
2025-02-08 16:25:18 +0100

    cleanup

--- Assets/Scripts/TerrainGen/MemoryCleaner.cs
+++ Assets/Scripts/TerrainGen/MemoryCleaner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChunkStorage : MonoBehaviour
+public class MemoryCleaner : MonoBehaviour
 {
 	public float maxVoxelGridMapSizeMB;
 
